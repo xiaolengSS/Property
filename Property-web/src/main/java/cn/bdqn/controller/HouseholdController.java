fa3f43d954995c3868cdeb81d3bac9ba7687fd6f
JinSuiSheng <2837297114@qq.com>
2021-05-28 14:25:01 +0800

    更新

--- Property-web/src/main/java/cn/bdqn/controller/HouseholdController.java
+++ Property-web/src/main/java/cn/bdqn/controller/HouseholdController.java
@@ -2,6 +2,7 @@ package cn.bdqn.controller;
 
 import cn.bdqn.domain.Household;
 import cn.bdqn.service.HouseholdService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/househoid")
+@CrossOrigin
 public class HouseholdController {
 
     @Resource
