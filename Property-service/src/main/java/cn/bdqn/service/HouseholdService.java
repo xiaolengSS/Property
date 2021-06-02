7ba8b6c59c03d0f90d320835501bafc21cb5733c
laobao <laobao@qq.com>
2021-06-02 17:18:42 +0800

    停停停

---
+++ Property-service/src/main/java/cn/bdqn/service/HouseholdService.java
@@ -0,0 +1,23 @@
+package cn.bdqn.service;
+
+import cn.bdqn.domain.Household;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface HouseholdService {
+
+    //根据条件分页查询业主
+    public PageInfo<Household> queryAllByHouseholdPage(Integer currentPage, Integer pageSize,Household household);
+    //根据id查询数据
+    public Household queryHousehold(Household household);
+    //添加业主
+    public int saveHousehold(Household household);
+    //根据id修改业主
+    public void updateHousehold(Household household);
+    //根据id删除业主
+    public void deleteHousehold(Integer id);
+
+}
---
+++ Property-service/src/main/java/cn/bdqn/service/impl/HouseholdServiceImpl.java
@@ -0,0 +1,58 @@
+package cn.bdqn.service.impl;
+
+import cn.bdqn.domain.Household;
+import cn.bdqn.mapper.HouseholdMapper;
+import cn.bdqn.service.HouseholdService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service("householdService")
+public class HouseholdServiceImpl implements HouseholdService {
+
+    @Autowired
+    private HouseholdMapper householdMapper;
+
+    //根据条件分页查询业主
+    @Override
+    public PageInfo<Household> queryAllByHouseholdPage(Integer currentPage, Integer pageSize,Household household) {
+
+
+        PageHelper.startPage(currentPage,pageSize);
+
+        List<Household> households = householdMapper.selectHousehold(household);
+
+        PageInfo<Household> pageInfo = new PageInfo<Household>(households);
+
+
+        return pageInfo;
+    }
+    //查询单条业主信息
+    @Override
+    public Household queryHousehold(Household household) {
+
+        List<Household> households = householdMapper.selectHousehold(household);
+        Household household1 = (Household) households.get(0);
+
+        return household1;
+    }
+
+    //添加业主
+    @Override
+    public int saveHousehold(Household household) {
+        int i = householdMapper.insertHousehold(household);
+        return i;
+    }
+    //根据id修改业主
+    @Override
+    public void updateHousehold(Household household) {
+        householdMapper.updateHousehold(household);
+    }
+    //根据id删除业主
+    @Override
+    public void deleteHousehold(Integer id) {
+        householdMapper.deleteHousehold(id);
+    }
+}
