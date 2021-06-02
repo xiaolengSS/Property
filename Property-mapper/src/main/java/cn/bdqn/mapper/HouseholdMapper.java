9d17b566a18000804dfc4173ccec46ddf10720e9
laobao <laobao@qq.com>
2021-06-02 17:20:20 +0800

    你你你

---
+++ Property-mapper/src/main/java/cn/bdqn/mapper/HouseholdMapper.java
@@ -0,0 +1,16 @@
+package cn.bdqn.mapper;
+
+import cn.bdqn.domain.Household;
+
+import java.util.List;
+
+public interface HouseholdMapper {
+    //查询全部信息&根据条件单个或多个查询
+    public List<Household> selectHousehold(Household household);
+    //添加业主
+    public int insertHousehold(Household household);
+    //根据id修改业主
+    public void updateHousehold(Household household);
+    //根据id删除业主
+    public void deleteHousehold(Integer id);
+}
